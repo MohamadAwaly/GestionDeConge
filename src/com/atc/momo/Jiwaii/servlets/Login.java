d6cf59d524c02544b9e67e0ed0b753c9dda33238
Awaly Mohamad <awaly.mohamad@gmail.com>
2020-05-02 16:23:14 +0200

    Creation de fichier resouces

--- src/com/atc/momo/Jiwaii/servlets/Login.java
+++ src/com/atc/momo/Jiwaii/servlets/Login.java
@@ -7,15 +7,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet( name = "Login" )
+@WebServlet( name = "/Login" )
 public class Login extends HttpServlet {
+
+    public static final String emailUser      = "email";
+    public static final String motDePasseUser = "pass";
+    public static final String VUE_LOGIN      = "/resources/view/connexion.jsp";
+    public static final String VUE            = "/resources/view/accueil.jsp";
+
     protected void doPost( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
+        /**
+         * Récupération des données saisie, envoyées en temt que paramètre de la requête Post
+         * générer a la validation du formulaire
+         */
+       /* String email = request.getParameter( emailUser );
+        String motDePasse = request.getParameter( motDePasseUser );*/
 
     }
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException {
+        /**
+         * Affchage de la page Login
+         */
+        this.getServletContext().getRequestDispatcher( VUE_LOGIN ).forward( request, response );
 
     }
 }
--- web/resources/view/connexion.jsp
+++ web/resources/view/connexion.jsp
@@ -6,22 +6,18 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--Ajout de la biblioteque core --%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <title>Gestion de Conge</title>
-    <link rel="stylesheet" type="text/css" href="../style/styleConnexion.css">
+    <link rel="stylesheet" type="text/css" href="../css/styleConnexion.css">
 </head>
 <body>
-
-<header>
-    <h1>Gestion de congé</h1>
-</header>
-
+<c:import url="header.jsp"/>
 <form method="post" action="accueil" class="formulaireLogin">
-
-
     <legend>BIENVENUE</legend>
     <p>Connexion</p>
     <input type="email" name="email" id="email" class="ChampFormulaire" placeholder="E-mail"/></br></br>
