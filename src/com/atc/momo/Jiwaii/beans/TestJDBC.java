d8587f373cb8f1a2bc053c0bc16bb52188342dc5
Awaly Mohamad <awaly.mohamad@gmail.com>
2020-05-16 20:38:08 +0200

    modification dans le bean de TestJDBC

--- src/com/atc/momo/Jiwaii/beans/TestJDBC.java
+++ src/com/atc/momo/Jiwaii/beans/TestJDBC.java
@@ -1,6 +1,5 @@
 package com.atc.momo.Jiwaii.beans;
 
-import com.atc.momo.Jiwaii.servlets.Login;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -13,7 +12,7 @@ public class TestJDBC {
 
     /* La liste qui contiendra tous les resultats de nos essais */
     private      List<String> messages = new ArrayList<String>();
-    final static Logger       logger   = Logger.getLogger( Login.class );
+    final static Logger       logger2  = Logger.getLogger( TestJDBC.class );
 
     public List<String> executerTests( HttpServletRequest request ) {
         /* Chargement du driver JDBC pour MySQL */
@@ -27,9 +26,9 @@ public class TestJDBC {
         }
 
         /* Connexion à la base de données */
-        String url = "jdbc:mysql://localhost:3306/bdd_sdzee";
+        String url = "jdbc:mysql://localhost:3306/gestiondeconge";
         String utilisateur = "java";
-        String motDePasse = "SdZ_eE";
+        String motDePasse = "123456";
         Connection connexion = null;
         Statement statement = null;
         ResultSet resultat = null;
@@ -46,20 +45,27 @@ public class TestJDBC {
             resultat = statement.executeQuery( "SELECT id, email, motDePasse FROM User;" );
             messages.add( "Requête \"SELECT id, email, motDePAsse FROM User;\" effectuée !" );
 
+            try {
+                logger2.log( Level.INFO, "resultat" + resultat );
+                logger2.log( Level.INFO, "avant le while" );
+            } catch ( Exception e ){
+                logger2.fatal( "Une exceptin est survenue " + e.getMessage() );
+            }
+
             /* Récupération des données du résultat de la requête de lecture */
             while ( resultat.next() ) {
                 int idUtilisateur = resultat.getInt( "id" );
                 String emailUtilisateur = resultat.getString( "email" );
-                String motDePasseUtilisateur = resultat.getString( "mot_de_passe" );
+                String motDePasseUtilisateur = resultat.getString( "motDePasse" );
 
                 /* Formatage des données pour affichage dans la JSP finale. */
                 messages.add(
                         "Données retournées par la requête : id = " + idUtilisateur + ", email = " + emailUtilisateur
                                 + ", motdepasse = "
                                 + motDePasseUtilisateur + ", nom = " + "." );
-                logger.log( Level.INFO, idUtilisateur );
-                logger.log( Level.INFO, emailUtilisateur );
-                logger.log( Level.INFO, motDePasseUtilisateur );
+                //logger2.log( Level.INFO, idUtilisateur );
+                // logger2.log( Level.INFO, emailUtilisateur );
+                // logger2.log( Level.INFO, motDePasseUtilisateur );
             }
 
         } catch ( SQLException e ) {
